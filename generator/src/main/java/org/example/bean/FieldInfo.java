5de359bf9a31eec11d47ca49405d26a09cffd1bf
WhimsyQuester <rongquanhuang01@gmail.com>
2023-11-18 02:50:33 +0800

    Define field information of database table

---
+++ generator/src/main/java/org/example/bean/FieldInfo.java
@@ -0,0 +1,73 @@
+package org.example.bean;
+
+/**
+ * @Project: generator
+ * @Package: org.example.bean
+ * @ClassName: FieldInfo
+ * @Datetime: 2023/11/01 10:29
+ * @Author: HuangRongQuan
+ * @Email: rongquanhuang01@gmail.com
+ * @Description: 数据库表的字段信息
+ */
+
+public class FieldInfo {
+    // 字段名称
+    private String FieldName;
+    // bean属性名称
+    private String propertyName;
+    private String sqlType;
+    // 字段类型
+    private String javaType;
+    // 字段备注
+    private String comment;
+    // 字段是否是自增长
+    private Boolean isAutoIncrement;
+
+    public String getFieldName() {
+        return FieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        FieldName = fieldName;
+    }
+
+    public String getPropertyName() {
+        return propertyName;
+    }
+
+    public void setPropertyName(String propertyName) {
+        this.propertyName = propertyName;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Boolean getAutoIncrement() {
+        return isAutoIncrement;
+    }
+
+    public void setAutoIncrement(Boolean autoIncrement) {
+        isAutoIncrement = autoIncrement;
+    }
+
+    public String getSqlType() {
+        return sqlType;
+    }
+
+    public void setSqlType(String sqlType) {
+        this.sqlType = sqlType;
+    }
+
+    public String getJavaType() {
+        return javaType;
+    }
+
+    public void setJavaType(String javaType) {
+        this.javaType = javaType;
+    }
+}
