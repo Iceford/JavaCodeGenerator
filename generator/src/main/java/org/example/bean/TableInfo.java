0dc82a7bd7061145470af1e95b1a4ca121aa0fde
WhimsyQuester <rongquanhuang01@gmail.com>
2023-11-18 02:51:43 +0800

    Define information classes for database tables

---
+++ generator/src/main/java/org/example/bean/TableInfo.java
@@ -0,0 +1,108 @@
+package org.example.bean;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Project: generator
+ * @Package: org.example.bean
+ * @ClassName: TableInfo
+ * @Datetime: 2023/11/01 10:29
+ * @Author: HuangRongQuan
+ * @Email: rongquanhuang01@gmail.com
+ * @Description: 数据库表的信息
+ */
+
+public class TableInfo {
+    // 表名
+    private String tableName;
+    // bean名称
+    private String beanName;
+    // 参数名称
+    private String beanParamName;
+    // 表注释
+    private String comment;
+    // 字段信息
+    private List<FieldInfo> fieldList;
+    // 唯一索引集合
+    private Map<String, List<FieldInfo>> keyIndexMap = new LinkedHashMap();
+    // 是否有date类型
+    private Boolean haveDate;
+    // 是否有时间类型
+    private Boolean haveDateTime;
+    // 是否有 bigdecimal类型
+    private Boolean haveBigDecimal;
+
+    public Boolean getHaveBigDecimal() {
+        return haveBigDecimal;
+    }
+
+    public void setHaveBigDecimal(Boolean haveBigDecimal) {
+        this.haveBigDecimal = haveBigDecimal;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+
+    public String getBeanParamName() {
+        return beanParamName;
+    }
+
+    public void setBeanParamName(String beanParamName) {
+        this.beanParamName = beanParamName;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public List<FieldInfo> getFieldList() {
+        return fieldList;
+    }
+
+    public void setFieldList(List<FieldInfo> fieldList) {
+        this.fieldList = fieldList;
+    }
+
+    public Map<String, List<FieldInfo>> getKeyIndexMap() {
+        return keyIndexMap;
+    }
+
+    public void setKeyIndexMap(Map<String, List<FieldInfo>> keyIndexMap) {
+        this.keyIndexMap = keyIndexMap;
+    }
+
+    public Boolean getHaveDate() {
+        return haveDate;
+    }
+
+    public void setHaveDate(Boolean haveDate) {
+        this.haveDate = haveDate;
+    }
+
+    public Boolean getHaveDateTime() {
+        return haveDateTime;
+    }
+
+    public void setHaveDateTime(Boolean haveDateTime) {
+        this.haveDateTime = haveDateTime;
+    }
+}
